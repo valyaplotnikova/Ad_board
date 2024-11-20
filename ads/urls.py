2fd0a73b137f21fe282216205a0b39b0dbf62175
valya_plotnikova <valya_ezhova@mail.ru>
2024-11-20 15:51:13 +0300

    update files with permissions

--- ads/urls.py
+++ ads/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from ads.apps import AdsConfig
-from ads.views import AdListAPIView, AdCreateAPIView, AdRetrieveAPIView, AdUpdateAPIView, AdDestroyAPIView
+from ads.views import AdListAPIView, AdCreateAPIView, AdRetrieveAPIView, AdUpdateAPIView, AdDestroyAPIView, \
+    ReviewListAPIView, ReviewCreateAPIView, ReviewRetrieveAPIView, ReviewUpdateAPIView, ReviewDestroyAPIView
 
 app_name = AdsConfig.name
 
@@ -11,4 +12,9 @@ urlpatterns = [
     path('ads/get/<int:pk>/', AdRetrieveAPIView.as_view(), name='ad_get'),
     path('ads/update/<int:pk>/', AdUpdateAPIView.as_view(), name='ad_update'),
     path('ads/delete/<int:pk>/', AdDestroyAPIView.as_view(), name='ad_delete'),
+    path('review/', ReviewListAPIView.as_view(), name='review_list'),
+    path('review/create/', ReviewCreateAPIView.as_view(), name='review_create'),
+    path('review/get/<int:pk>/', ReviewRetrieveAPIView.as_view(), name='review_get'),
+    path('review/update/<int:pk>/', ReviewUpdateAPIView.as_view(), name='review_update'),
+    path('review/delete/<int:pk>/', ReviewDestroyAPIView.as_view(), name='review_delete'),
 ]
--- ads/views.py
+++ ads/views.py
@@ -1,30 +1,70 @@
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from django_filters import OrderingFilter
 
-from ads.models import Ad
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from ads.models import Ad, Review
 from ads.pagination import AdPagination
-from ads.serializers import AdSerializer
+from ads.serializers import AdSerializer, ReviewSerializer
+from users.permissions import IsAuthor, IsAdmin
 
 
 class AdListAPIView(ListAPIView):
     serializer_class = AdSerializer
     queryset = Ad.objects.all()
     pagination_class = AdPagination
+    filter_backends = [OrderingFilter]
+    search_fields = ['title']
+    permission_classes = (AllowAny,)
 
 
 class AdCreateAPIView(CreateAPIView):
     serializer_class = AdSerializer
     queryset = Ad.objects.all()
+    permission_classes = (IsAuthenticated, IsAdmin | IsAuthor)
 
 
 class AdRetrieveAPIView(RetrieveAPIView):
     serializer_class = AdSerializer
     queryset = Ad.objects.all()
+    permission_classes = (IsAuthenticated, IsAdmin | IsAuthor)
 
 
 class AdUpdateAPIView(UpdateAPIView):
     serializer_class = AdSerializer
     queryset = Ad.objects.all()
+    permission_classes = (IsAuthenticated, IsAdmin | IsAuthor)
 
 
 class AdDestroyAPIView(DestroyAPIView):
     queryset = Ad.objects.all()
+    permission_classes = (IsAuthenticated, IsAdmin | IsAuthor)
+
+
+class ReviewListAPIView(ListAPIView):
+    serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
+    permission_classes = (IsAuthenticated, IsAdmin | IsAuthor)
+
+
+class ReviewCreateAPIView(CreateAPIView):
+    serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
+    permission_classes = (IsAuthenticated, IsAdmin | IsAuthor)
+
+
+class ReviewRetrieveAPIView(RetrieveAPIView):
+    serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
+    permission_classes = (IsAuthenticated, IsAdmin | IsAuthor)
+
+
+class ReviewUpdateAPIView(UpdateAPIView):
+    serializer_class = ReviewSerializer
+    queryset = Review.objects.all()
+    permission_classes = (IsAuthenticated, IsAdmin | IsAuthor)
+
+
+class ReviewDestroyAPIView(DestroyAPIView):
+    queryset = Review.objects.all()
+    permission_classes = (IsAuthenticated, IsAdmin | IsAuthor)
